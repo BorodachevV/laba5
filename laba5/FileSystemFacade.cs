81f3cec81ab6e186de2312ccee86fab3cf041adb
BorodachevV <borodachevvs@mer.ci.nsu.ru>
2026-03-12 11:37:15 +0700

    Update FileSystemFacade.cs

--- laba5/FileSystemFacade.cs
+++ laba5/FileSystemFacade.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,42 +6,20 @@ namespace laba5
 {
     public class FileSystemFacade
     {
-        private readonly IStorageAdapter _localDb;
-        private readonly IStorageAdapter _cloudDb;
-
-        public FileSystemFacade(IStorageAdapter localDb, IStorageAdapter cloudDb)
-        {
-            _localDb = localDb;
-            _cloudDb = cloudDb;
+        private readonly IFileSystem _fileSystem;
+        public FileSystemFacade(IFileSystem fileSystem) { _fileSystem = fileSystem; }
+        public void ReadSystemConfigs(List<string> configPaths) { 
+            Console.WriteLine("\n=== [Фасад] Запуск чтения системных конфигураций ==="); 
+            foreach (var path in configPaths) { 
+                _fileSystem.Read(path); } 
+            Console.WriteLine("=== [Фасад] Чтение завершено ===\n"); 
         }
-
-        public void BackupLocalToCloud(IFileSystemComponent rootNode)
-        {
-            Console.WriteLine("\n=== ЗАПУСК РЕЗЕРВНОГО КОПИРОВАНИЯ В ОБЛАКО ===");
-            Console.WriteLine($"Подсчет требуемого места: {rootNode.GetSize()} байт");
-
-            SyncRecursive(rootNode, "");
-
-            Console.WriteLine("=== РЕЗЕРВНОЕ КОПИРОВАНИЕ ЗАВЕРШЕНО ===\n");
-        }
-
-        private void SyncRecursive(IFileSystemComponent node, string currentPath)
-        {
-            string path = string.IsNullOrEmpty(currentPath) ? node.Name : $"{currentPath}/{node.Name}";
-
-            if (node is FileItem file)
-            {
-                string data = _localDb.Read(path);
-                _cloudDb.Write(path, data);
-            }
-            else if (node is FolderItem folder)
-            {
-                Console.WriteLine($"[Фасад] Синхронизация директории: {path}");
-                foreach (var child in folder.Children)
-                {
-                    SyncRecursive(child, path);
-                }
-            }
+        public void BatchCleanup(List<string> pathsToDelete) { 
+            Console.WriteLine("\n=== [Фасад] Запуск пакетной очистки файлов ==="); 
+            foreach (var path in pathsToDelete) { 
+                _fileSystem.Delete(path); 
+            } 
+            Console.WriteLine("=== [Фасад] Пакетная очистка завершена ===\n"); 
         }
     }
 }
